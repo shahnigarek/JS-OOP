c8e3045bf93f909b74fef648d836d51a4550344d
shahnigarek <shahnigarek@code.edu.az>
2022-09-13 08:20:20 +0400

    completed

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// INHERITANCE
+// var person={
+//     firstname:"Unknown",
+//     lastname:"Unknown",
+//     getFullName:function(){
+//        return this.firstname + " " + this.lastname;
+//     }
+// }
+// var human={
+//     firstname:"Arzu",
+//     lastname:"Aliyeva"
+// }
+// human.__proto__=person
+// console.log(human.getFullName());
+// ENCAPSULATION
+// var teacher={
+//     name:"Arzu"
+// }
+// console.log(teacher.name);
+// teacher.name="Ali"
+// console.log(teacher.name);
+// teacher.name="Sevinc"
+// console.log(teacher.name);
+// Task1
+
+// Task2
+// function multiplyBy()
+// {
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+//         document.getElementById("result").innerHTML = num1 * num2;
+// }
